6b8b89cdd62224ac9fca54efcb226b92a0748d28
Imercfy-Labs <tools@imercfy.com>
2025-05-16 01:53:37 +0530

    Updated api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://devkicl.duckdns.org/api/login';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://devkicl.duckdns.org';
 
 interface ApiResponse<T> {
   data?: T;
